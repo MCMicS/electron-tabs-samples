42b5eaae312efb14f5982d32191316a826edbc5b
mcmics <MCMicS@users.noreply.github.com>
2022-04-12 07:21:47 +0200

    open Window in recnderer

--- main.js
+++ main.js
@@ -1,63 +1,40 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain, webContents} = require('electron')
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
 function createWindow () {
-  const mainWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false, // needed for Electron >= 12.x
-      webviewTag: true
-    }
-  });
+    const mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false, // needed for Electron >= 12.x
+            webviewTag: true
+        }
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    // and load the index.html of the app.
+    mainWindow.maximize()
+    mainWindow.show()
+    mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  ipcMain.on('new-tab', (event, webContentsId) => {
-    const tabWebContents = webContents.fromId(webContentsId)
-    tabWebContents.setWindowOpenHandler((details) => {
-      console.log(`Handle ${details.url} as ${details.disposition} for ${tabWebContents.id}`)
-      switch(details.disposition) {
-          case "foreground-tab":
-              mainWindow.webContents.send('open-tab', {url: details.url, active: true})
-              break
-          case "background-tab":
-              mainWindow.webContents.send('open-tab', {url: details.url, active: false})
-              break
-          case "new-window":
-              return {
-                  action: 'allow'
-              }
-
-      }
-      return { action: 'deny' }
-    })
-  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+    createWindow()
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+
+    app.on('web-contents-created', function (webContentsCreatedEvent, contents) {
+        if (contents.getType() === 'webview') {
+            contents.on('new-window', function (newWindowEvent, url) {
+                console.log('block - handled in renderer.js');
+                newWindowEvent.preventDefault();
+            });
+        }
+    });
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,49 +1,27 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
-
 const TabGroup = require('electron-tabs')
-const ipcRenderer = require("electron").ipcRenderer;
 
-let tabGroup = new TabGroup({
-    newTab: {
-        title: 'New Tab'
-    }
-});
-tabGroup.on("tab-added", (tab, tabGroup) => {
-    tab.once("webview-ready", t => ipcRenderer.send('new-tab', t.webview.getWebContentsId()))
-})
+let tabGroup = new TabGroup();
 
 tabGroup.addTab({
     title: "Electron",
-    src: "https://www.electronjs.org/",
-    visible: true
-});
-
-tabGroup.addTab({
-    title: "Allow Popups",
-    src: "./allowPopups.html",
+    src: "./demo.html",
     visible: true,
     active: true,
     webviewAttributes: {
-        'allowpopups': true
+        allowpopups: true,
+        id:'foo',
     }
 });
 
-function openTab(tabInfo) {
-    console.log(`Open Tab ${tabInfo.url} as active --> ${tabInfo.active}`)
-    tabGroup.addTab({
-        title: tabInfo.title || "From Popup",
-        src: tabInfo.url,
-        visible: true,
-        active: tabInfo.active,
-        webviewAttributes: {
-            'allowpopups': true
-        }
-    });
-}
+const webview = document.getElementById('foo')
 
-ipcRenderer.on('open-tab', (event, tabInfo) => openTab(tabInfo))
+webview.addEventListener('new-window', (e) => {
+    e.preventDefault()
+    tabGroup.addTab({
+        title: "new page",
+        src: e.url,
+        active: true,
+    });
+
+});
+
